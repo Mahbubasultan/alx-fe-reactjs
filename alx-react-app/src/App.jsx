82dcf79d465efa79e0988c861b56f205f60fc420
Mahbuba sultan <msultanf005@gmail.com>
2025-11-01 08:49:45 +0200

    Organize components and add UserProfile with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
-
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
       <Header />
+      <WelcomeMessage />
       <MainContent />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age={30} bio="Enjoys cooking and traveling" />
+      <UserProfile name="Charlie" age={28} bio="Fan of movies and music" />
+
       <Footer />
     </>
   );
